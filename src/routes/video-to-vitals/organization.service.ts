b3bb022bb9440d3aa5f9f95035f60a3a81ddfb0c
mohammedamraz <mohammed.amraz@fedo.ai>
2022-09-07 10:50:38 +0530

    added paginator

--- src/routes/video-to-vitals/organization.service.ts
+++ src/routes/video-to-vitals/organization.service.ts
@@ -107,6 +107,26 @@ export class OrganizationService {
         )
       )
     }
+
+    //this function can be used for the paginator function that we are using in the list of datas in the admin panel 
+    Paginator(items:any, page:any, per_page:any) {
+
+      var page = page || 1,
+      per_page = per_page || 10,
+      offset = (page - 1) * per_page,
+    
+      paginatedItems = items.slice(offset).slice(0, per_page),
+      total_pages = Math.ceil(items.length / per_page);
+      return {
+      page: page,
+      per_page: per_page,
+      pre_page: page - 1 ? page - 1 : null,
+      next_page: (total_pages > page) ? page + 1 : null,
+      total: items.length,
+      total_pages: total_pages,
+      data: paginatedItems
+      };
+    }
   
   
     patchImageToOrganization(id:number,path:any){
