99df64271ba262f8f1679a80cc14e7b54699ae4d
mohammedamraz <mohammed.amraz@fedo.ai>
2022-09-25 22:53:50 +0530

    added interceptors in place of aws

--- src/routes/video-to-vitals/video-to-vitals.controller.ts
+++ src/routes/video-to-vitals/video-to-vitals.controller.ts
@@ -6,6 +6,7 @@ import { PasswordResetDTO } from '../admin/dto/create-admin.dto';
 import { ConfirmForgotPasswordDTO, ForgotPasswordDTO } from '../admin/dto/login.dto';
 import { OrganizationService } from './organization.service';
 import { UsersService } from './users.service';
+import { LoggingInterceptor } from 'src/interceptors/interceptor';
 
 
 const APP = "VideoToVitalsController"
@@ -211,7 +212,7 @@ export class VideoToVitalsController {
   }
 
   @Post('signup/user')
-  // @UseInterceptors(LoggingInterceptor)
+  @UseInterceptors(LoggingInterceptor)
   registerUserbyEmail(@Body() RegisterUserdto: RegisterUserDTO) {
     Logger.debug(`registerUserbyEmail()  loginUserDTO:${JSON.stringify(RegisterUserdto)} `, APP);
 
@@ -219,7 +220,7 @@ export class VideoToVitalsController {
   }
 
   @Post('confirm/signup')
-  // @UseInterceptors(LoggingInterceptor)
+  @UseInterceptors(LoggingInterceptor)
   confirmSignupUserByEmail(@Body() registerUserdto: RegisterUserDTO) {
     Logger.debug(`confirmSignupUserByEmail() [${JSON.stringify(Object.keys(registerUserdto))} values ${JSON.stringify(Object.values(registerUserdto).length)}]`, APP);
 
@@ -227,7 +228,7 @@ export class VideoToVitalsController {
   }
 
   @Post('login/user')
-  // @UseInterceptors(LoggingInterceptor)
+  @UseInterceptors(LoggingInterceptor)
   loginUserByEmail(@Body() loginUserDTO: LoginUserDTO) {
     Logger.debug(`loginUserByEmail()  loginUserDTO:${JSON.stringify(loginUserDTO)} `, APP);
 
@@ -250,7 +251,7 @@ export class VideoToVitalsController {
   }
 
   @Post('password')
-  // @UseInterceptors(LoggingInterceptor)
+  @UseInterceptors(LoggingInterceptor)
   forgotPassword(@Body() forgotPasswordDTO: ForgotPasswordDTO) {
     Logger.debug(`forgotPassword() forgotPasswordDTO:[${JSON.stringify(forgotPasswordDTO,)}]`,);
 
@@ -258,7 +259,7 @@ export class VideoToVitalsController {
   }
 
   @Post('password/otp')
-  // @UseInterceptors(LoggingInterceptor)
+  @UseInterceptors(LoggingInterceptor)
   confirmForgotPassword(@Body() confirmForgotPasswordDTO: ConfirmForgotPasswordDTO,) {
     Logger.debug(`confirmForgotPassword() confirmForgotPasswordDTO:[${JSON.stringify(confirmForgotPasswordDTO,)}]`,);
 
